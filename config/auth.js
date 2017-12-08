da2a0fd6986114aab7d8e4642f114c29b2df7960
OH SAEJUNG <tpwjd9494@naver.com>
2017-12-09 00:09:20 +0900

    'face'

--- config/auth.js
+++ config/auth.js
@@ -1,81 +1,68 @@
-var LocalStrategy = require('passport-local').Strategy,
-    FacebookStrategy = require('passport-facebook').Strategy,
-    User = require('../models/User'); 
+const LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const User = require('../models/User');
 
 module.exports = function(passport) {
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
+  passport.deserializeUser((id, done) =>  {
+    User.findById(id, done);
   });
 
   passport.use('local-signin', new LocalStrategy({
     usernameField : 'email',
     passwordField : 'password',
     passReqToCallback : true
-  }, function(req, email, password, done) {
-    process.nextTick(function () {
-      User.findOne({email: email}, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-
-        if (!user) {
-          return done(null, false, req.flash('danger', '존재하지 않는 사용자입니다.'));
-        }
-
-        if (!user.validatePassword(password)) {
-          return done(null, false, req.flash('danger', '비밀번호가 일치하지 않습니다.'));
-        }
-
-        return done(null, user, req.flash('success', '로그인되었습니다.'));
-      });
-    });
+  }, async (req, email, password, done) => {
+    try {
+      const user = await User.findOne({email: email});
+      if (user && await user.validatePassword(password)) {
+        return done(null, user, req.flash('success', 'Welcome!'));
+      }
+      return done(null, false, req.flash('danger', 'Invalid email or password'));
+    } catch(err) {
+      done(err);
+    }
   }));
 
   passport.use(new FacebookStrategy({
+    // 이 부분을 여러분 Facebook App의 정보로 수정해야 합니다.
     clientID : '2037750849791001',
     clientSecret : '3e144ae0337664c466996e4d4aa5d59e',
-    callbackURL : 'https://pure-sea-42862.herokuapp.com/auth/facebook/callback',
-    profileFields : ["name", "displayName", "emails", "photos", "id"]
-    
-  }, function(token, refreshToken, profile, done) {
-    console.log(profile);
-    var email = profile.emails[0].value;
-    process.nextTick(function () {
-      User.findOne({'facebook.id': profile.id}, function(err, user) {
-        if (err) {
-          return done(err);
+    callbackURL : 'http://localhost:3000/auth/facebook/callback',
+    profileFields : ['email', 'name', 'picture']
+  }, async (token, refreshToken, profile, done) => {
+    console.log('Facebook', profile); // profile 정보로 뭐가 넘어오나 보자.
+    try {
+      var email = (profile.emails && profile.emails[0]) ? profile.emails[0].value : '';
+      var picture = (profile.photos && profile.photos[0]) ? profile.photos[0].value : '';
+      var name = (profile.displayName) ? profile.displayName : 
+        [profile.name.givenName, profile.name.middleName, profile.name.familyName]
+          .filter(e => e).join(' ');
+      console.log(email, picture, name, profile.name);
+      // 같은 facebook id를 가진 사용자가 있나?
+      var user = await User.findOne({'facebook.id': profile.id});
+      if (!user) {
+        // 없다면, 혹시 같은 email이라도 가진 사용자가 있나?
+        if (email) {
+          user = await User.findOne({email: email});
         }
-        if (user) {
-          return done(null, user);
-        } else {
-          User.findOne({email: email}, function(err, user) {
-            if (err) {
-              return done(err);
-            }
-            if (!user) {
-              user = new User({
-                name: profile.displayName,
-                email: profile.emails[0].value
-              });
-            }
-            user.facebook.id = profile.id;
-            user.facebook.token = profile.token;
-            user.facebook.photo = profile.photos[0].value;
-            user.save(function(err) {
-              if (err) {
-                return done(err);
-              }
-              return done(null, user);
-            });
-          });
+        if (!user) {
+          // 그것도 없다면 새로 만들어야지.
+          user = new User({name: name});
+          user.email =  email ? email : `__unknown-${user._id}@no-email.com`;
         }
-      });
-    });
+        // facebook id가 없는 사용자는 해당 id를 등록
+        user.facebook.id = profile.id;
+        user.facebook.photo = picture;
+      }
+      user.facebook.token = profile.token;
+      await user.save();
+      return done(null, user);
+    } catch (err) {
+      done(err);
+    }
   }));
 };
